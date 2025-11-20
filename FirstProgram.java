50b7303d98a5b4999b4aa00d9bc03395fb07b79b
dev global <zamil@gmail.com>
2025-11-20 21:11:46 +0530

    first day git

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,5 +1,7 @@
 public class FirstProgram {
-    public static void main(String[] args) {
+    public static void main(String[] myArgument) {
         System.out.println("Hello World");
+        System.out.println("my name is \t"+myArgument[0]);
+        System.out.println("I am "+myArgument[1]+" years old");
     }
 }
