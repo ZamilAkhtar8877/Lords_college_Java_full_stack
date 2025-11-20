33bd45f6430a26dfaa241053a8ee425dd967439a
dev global <zamil@gmail.com>
2025-11-20 21:15:31 +0530

    just a change

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,7 +1,7 @@
 public class FirstProgram {
     public static void main(String[] myArgument) {
         System.out.println("Hello World");
-        System.out.println("my name is \t"+myArgument[0]);
+        System.out.println("my name is "+myArgument[0]);
         System.out.println("I am "+myArgument[1]+" years old");
     }
 }
